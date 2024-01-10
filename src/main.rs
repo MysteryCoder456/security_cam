a6ff12b972ca74f33228534b038896b320d2bb8b
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-01-10 18:48:16 +0400

    tcp echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net,
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = net::TcpListener::bind("0.0.0.0:7020").await?;
+
+    loop {
+        let (mut sock, addr) = listener.accept().await?;
+        println!("New connection from {:?}", addr);
+
+        let mut buf = vec![0; 1024];
+        let bytes_read = sock.read(&mut buf).await?;
+
+        println!("Echoing {} bytes back", bytes_read);
+        sock.write(&buf).await?;
+    }
 }
