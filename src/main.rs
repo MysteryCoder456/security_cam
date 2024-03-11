e2ea1de2ce44040ade62777fb00187c74f7e6a78
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-03-11 13:29:22 +0400

    finally got opencv to work

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
+use opencv::{highgui, prelude::*, videoio};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net,
 };
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let listener = net::TcpListener::bind("0.0.0.0:7020").await?;
+async fn main() -> anyhow::Result<()> {
+    let window = "camera";
+    highgui::named_window(window, highgui::WINDOW_AUTOSIZE)?;
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+
+    if !cam.is_opened()? {
+        panic!("Unable to open camera!");
+    }
+
+    //let listener = net::TcpListener::bind("0.0.0.0:7020").await?;
 
     loop {
-        let (mut sock, addr) = listener.accept().await?;
-        println!("New connection from {:?}", addr);
+        //let (mut sock, addr) = listener.accept().await?;
+        //println!("New connection from {:?}", addr);
 
-        let mut buf = vec![0; 1024];
-        let bytes_read = sock.read(&mut buf).await?;
+        //let mut buf = vec![0; 1024];
+        //let bytes_read = sock.read(&mut buf).await?;
 
-        println!("Echoing {} bytes back", bytes_read);
-        sock.write(&buf).await?;
+        //println!("Echoing {} bytes back", bytes_read);
+        //sock.write(&buf).await?;
+
+        let mut frame = Mat::default();
+        cam.read(&mut frame)?;
+        if frame.size()?.width > 0 {
+            highgui::imshow(window, &frame)?;
+        }
+
+        let key = highgui::wait_key(10)?;
+        if key > 0 && key != 255 {
+            break;
+        }
     }
+
+    Ok(())
 }
