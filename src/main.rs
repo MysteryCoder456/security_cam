5d41a417f777cfa24226f39e7f892c4f3346c468
Rehatbir Singh <rehatbir.singh@gmail.com>
2024-03-15 12:05:35 +0400

    servo control (currently doesn't work but idk man)

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,30 @@ use opencv::{
     prelude::*,
     videoio,
 };
-use tokio::{net, sync::broadcast};
+use rppal::pwm;
+use tokio::{
+    net,
+    sync::{broadcast, mpsc},
+};
 
 type FrameData = Vec<u8>;
 
+enum ServoPosition {
+    Left,
+    Center,
+    Right,
+    Custom(f32),
+}
+
 const MAX_FRAME_RATE: f32 = 30.;
 const IMG_FORMAT: &str = ".webp";
+const MAX_PULSE_WIDTH_US: u64 = 2000;
+const MIN_PULSE_WIDTH_US: u64 = 1000;
 
-fn footage_capture(footage_tx: Arc<broadcast::Sender<FrameData>>, mut cam: videoio::VideoCapture) {
+fn footage_capture_task(
+    footage_tx: Arc<broadcast::Sender<FrameData>>,
+    mut cam: videoio::VideoCapture,
+) {
     let mut frame = Mat::default();
     let mut resized_frame = Mat::default();
 
@@ -88,6 +104,25 @@ fn footage_capture(footage_tx: Arc<broadcast::Sender<FrameData>>, mut cam: video
     }
 }
 
+async fn servo_task(mut servo_rx: mpsc::UnboundedReceiver<ServoPosition>, servo_pwm: pwm::Pwm) {
+    loop {
+        while let Some(servo_pos) = servo_rx.recv().await {
+            let pulse_width = match servo_pos {
+                ServoPosition::Left => MIN_PULSE_WIDTH_US,
+                ServoPosition::Center => (MAX_PULSE_WIDTH_US + MIN_PULSE_WIDTH_US) / 2,
+                ServoPosition::Right => MAX_PULSE_WIDTH_US,
+                ServoPosition::Custom(pos) => {
+                    (pos * (MAX_PULSE_WIDTH_US - MIN_PULSE_WIDTH_US) as f32 / 2.) as u64
+                        + (MAX_PULSE_WIDTH_US + MIN_PULSE_WIDTH_US) / 2
+                }
+            };
+            servo_pwm
+                .set_pulse_width(Duration::from_micros(pulse_width))
+                .unwrap();
+        }
+    }
+}
+
 async fn index() -> impl IntoResponse {
     Html(include_str!("index.html"))
 }
@@ -95,16 +130,21 @@ async fn index() -> impl IntoResponse {
 async fn ws_handler(
     ws: WebSocketUpgrade,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
-    State(footage_tx): State<Arc<broadcast::Sender<FrameData>>>,
+    State((footage_tx, servo_tx)): State<(
+        Arc<broadcast::Sender<FrameData>>,
+        mpsc::UnboundedSender<ServoPosition>,
+    )>,
 ) -> impl IntoResponse {
-    let rx = footage_tx.subscribe();
-    ws.on_upgrade(move |sock| client_handler(sock, addr, rx))
+    let footage_rx = footage_tx.subscribe();
+    let servo_tx = servo_tx.clone();
+    ws.on_upgrade(move |sock| client_handler(sock, addr, footage_rx, servo_tx))
 }
 
 async fn client_handler(
     mut socket: WebSocket,
     addr: SocketAddr,
     mut footage_rx: broadcast::Receiver<FrameData>,
+    servo_tx: mpsc::UnboundedSender<ServoPosition>,
 ) {
     println!("New connection from {:?}", addr);
 
@@ -121,6 +161,18 @@ async fn client_handler(
             },
             Some(Ok(msg)) = socket.recv() => {
                 match msg {
+                    Message::Text(msg) => {
+                        let servo_pos = match msg.as_str() {
+                            "left" => Some(ServoPosition::Left),
+                            "center" => Some(ServoPosition::Center),
+                            "right" => Some(ServoPosition::Right),
+                            _ => msg.parse::<f32>().ok().map(|pos| ServoPosition::Custom(pos.clamp(-1.0, 1.0))),
+                        };
+
+                        if let Some(servo_pos) = servo_pos {
+                            servo_tx.send(servo_pos).unwrap();
+                        }
+                    },
                     Message::Close(_) => break,
                     _ => {}
                 }
@@ -133,24 +185,38 @@ async fn client_handler(
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
+    // Initialize camera
     let cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
     if !cam.is_opened()? {
         panic!("Unable to open camera!");
     }
 
+    // Initialize PWM servo
+    let servo_pwm = pwm::Pwm::with_period(
+        pwm::Channel::Pwm0,
+        Duration::from_millis(20),
+        Duration::from_micros((MAX_PULSE_WIDTH_US + MIN_PULSE_WIDTH_US) / 2),
+        pwm::Polarity::Normal,
+        true,
+    )?;
+
     // Spawn footage capture task
-    let (tx, _) = broadcast::channel::<FrameData>(1);
-    let tx = Arc::new(tx);
-    let tx_clone = tx.clone();
+    let (footage_tx, _) = broadcast::channel::<FrameData>(1);
+    let footage_tx = Arc::new(footage_tx);
+    let footage_tx_clone = footage_tx.clone();
     tokio::task::spawn_blocking(move || {
-        footage_capture(tx_clone, cam);
+        footage_capture_task(footage_tx_clone, cam);
     });
+
+    // Spawn servo movement task
+    let (servo_tx, servo_rx) = mpsc::unbounded_channel::<ServoPosition>();
+    tokio::task::spawn(servo_task(servo_rx, servo_pwm));
 
     // Make and serve HTTP server
     let app = Router::new()
         .route("/", get(index))
         .route("/ws", get(ws_handler))
-        .with_state(tx);
+        .with_state((footage_tx, servo_tx));
 
     let listener = net::TcpListener::bind("0.0.0.0:7020").await?;
     axum::serve(
