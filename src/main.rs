5285f16cba4731363f171147afbeeb499a427245
Rehatbir Singh <rehatbir.singh@gmail.com>
2024-03-18 19:53:43 +0400

    switched to libcamera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    io::Cursor,
     net::SocketAddr,
     sync::Arc,
     time::{Duration, Instant},
@@ -13,12 +14,16 @@ use axum::{
     routing::get,
     Router,
 };
-use cfg_if::cfg_if;
-use opencv::{
-    core::{Size, Vector},
-    imgcodecs, imgproc,
-    prelude::*,
-    videoio,
+use image::{ImageBuffer, ImageFormat, RgbImage};
+use libcamera::{
+    camera::CameraConfigurationStatus,
+    camera_manager::CameraManager,
+    framebuffer_allocator::{FrameBuffer, FrameBufferAllocator},
+    framebuffer_map::MemoryMappedFrameBuffer,
+    geometry::Size,
+    pixel_format::PixelFormat,
+    request::ReuseFlag,
+    stream::StreamRole,
 };
 use rppal::pwm;
 use tokio::{
@@ -36,27 +41,73 @@ enum ServoPosition {
 }
 
 const MAX_FRAME_RATE: f32 = 30.;
-const IMG_FORMAT: &str = ".webp";
+const PIXEL_FORMAT: PixelFormat = PixelFormat::new(u32::from_le_bytes([b'B', b'G', b'2', b'4']), 0);
 const MAX_PULSE_WIDTH_US: u64 = 2000;
 const MIN_PULSE_WIDTH_US: u64 = 1000;
 
-fn footage_capture_task(
-    footage_tx: Arc<broadcast::Sender<FrameData>>,
-    mut cam: videoio::VideoCapture,
-) {
-    let mut frame = Mat::default();
-    let mut resized_frame = Mat::default();
-
-    #[cfg(feature = "bgr2rgb")]
-    let mut rgb_frame = Mat::default();
-
-    let mut frame_data = Vector::new();
-    let params = {
-        let mut p = Vector::<i32>::new();
-        p.push(imgcodecs::IMWRITE_WEBP_QUALITY);
-        p.push(70);
-        p
+// FIXME: Video streaming is a lot faster, but results in some blank frames
+fn footage_capture_task(footage_tx: Arc<broadcast::Sender<FrameData>>) {
+    // Initialize camera
+    let cam_mgr = CameraManager::new().unwrap();
+    let cameras = cam_mgr.cameras();
+    let cam = cameras.get(0).unwrap();
+    let mut cam = cam.acquire().unwrap();
+    let size = Size {
+        width: 640,
+        height: 480,
     };
+
+    // Configure camera
+    let mut cfgs = cam
+        .generate_configuration(&[StreamRole::VideoRecording])
+        .unwrap();
+    cfgs.get_mut(0).unwrap().set_pixel_format(PIXEL_FORMAT);
+    cfgs.get_mut(0).unwrap().set_size(size);
+
+    match cfgs.validate() {
+        CameraConfigurationStatus::Valid => println!("Camera configuration valid!"),
+        CameraConfigurationStatus::Adjusted => {
+            println!("Camera configuration was adjusted: {:#?}", cfgs)
+        }
+        CameraConfigurationStatus::Invalid => panic!("Error validating camera configuration"),
+    }
+
+    cam.configure(&mut cfgs).unwrap();
+
+    // Allocate framebuffers
+    let mut alloc = FrameBufferAllocator::new(&cam);
+    let cfg = cfgs.get(0).unwrap();
+    let stream = cfg.stream().unwrap();
+    let buffers = alloc.alloc(&stream).unwrap();
+
+    let buffers = buffers
+        .into_iter()
+        .map(|buf| MemoryMappedFrameBuffer::new(buf).unwrap())
+        .collect::<Vec<_>>();
+
+    // Create requests and bind the allocated buffers
+    let reqs = buffers
+        .into_iter()
+        .enumerate()
+        .map(|(i, buf)| {
+            let mut req = cam.create_request(Some(i as u64)).unwrap();
+            req.add_buffer(&stream, buf).unwrap();
+            req
+        })
+        .collect::<Vec<_>>();
+
+    let (req_tx, req_rx) = std::sync::mpsc::channel();
+    cam.on_request_completed(move |req| {
+        req_tx.send(req).unwrap();
+    });
+
+    // Start camera and queue capture requests
+    cam.start(None).unwrap();
+    for req in reqs {
+        cam.queue_request(req).unwrap();
+    }
+
+    let mut converted_bytes = Vec::new();
 
     let mut start = Instant::now();
     let mut end = Instant::now();
@@ -72,33 +123,22 @@ fn footage_capture_task(
 
         start = Instant::now();
 
-        // Capture camera frame
-        cam.read(&mut frame).unwrap();
+        let mut req = req_rx.recv().unwrap();
 
-        // Resize to a smaller image
-        imgproc::resize(
-            &frame,
-            &mut resized_frame,
-            Size::new(1024, 576),
-            0.0,
-            0.0,
-            imgproc::INTER_LINEAR,
-        )
-        .unwrap();
+        let framebuffer: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
+        let planes = framebuffer.data();
 
-        // Convert color formats and encode image
-        cfg_if! {
-            if #[cfg(feature = "bgr2rgb")] {
-                imgproc::cvt_color(&resized_frame, &mut rgb_frame, imgproc::COLOR_BGR2RGB, 0).unwrap();
-                imgcodecs::imencode(IMG_FORMAT, &rgb_frame, &mut frame_data, &params).unwrap();
-            } else {
-                imgcodecs::imencode(IMG_FORMAT, &resized_frame, &mut frame_data, &params).unwrap();
-            }
-        }
+        let frame_data = planes.get(0).unwrap();
 
-        // Broadcast to network tasks
-        //println!("Frame size: {} bytes", frame_data.len());
-        _ = footage_tx.send(frame_data.to_vec());
+        let img: RgbImage =
+            ImageBuffer::from_raw(size.width, size.height, frame_data.to_vec()).unwrap();
+        img.write_to(&mut Cursor::new(&mut converted_bytes), ImageFormat::WebP)
+            .unwrap();
+
+        _ = footage_tx.send(converted_bytes.clone());
+
+        req.reuse(ReuseFlag::REUSE_BUFFERS);
+        cam.queue_request(req).unwrap();
 
         end = Instant::now();
     }
@@ -186,12 +226,6 @@ async fn client_handler(
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
-    // Initialize camera
-    let cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
-    if !cam.is_opened()? {
-        panic!("Unable to open camera!");
-    }
-
     // Initialize PWM servo
     let servo_pwm = pwm::Pwm::with_period(
         pwm::Channel::Pwm0,
@@ -206,7 +240,7 @@ async fn main() -> anyhow::Result<()> {
     let footage_tx = Arc::new(footage_tx);
     let footage_tx_clone = footage_tx.clone();
     tokio::task::spawn_blocking(move || {
-        footage_capture_task(footage_tx_clone, cam);
+        footage_capture_task(footage_tx_clone);
     });
 
     // Spawn servo movement task
