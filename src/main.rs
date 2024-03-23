98232aaa1e0ccee183962c8844805e49b885e189
Rehatbir Singh <rehatbir.singh@gmail.com>
2024-03-23 09:45:36 +0400

    separated footage code into footage.rs
    Signed-off-by: Rehatbir Singh <rehatbir.singh@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use std::{
-    io::Cursor,
-    net::SocketAddr,
-    sync::Arc,
-    time::{Duration, Instant},
-};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
 
 use axum::{
     extract::{
@@ -14,25 +9,15 @@ use axum::{
     routing::get,
     Router,
 };
-use drm_fourcc::DrmFourcc;
-use image::{ImageBuffer, ImageFormat, RgbImage};
-use libcamera::{
-    camera::CameraConfigurationStatus,
-    camera_manager::CameraManager,
-    framebuffer_allocator::{FrameBuffer, FrameBufferAllocator},
-    framebuffer_map::MemoryMappedFrameBuffer,
-    geometry::Size,
-    pixel_format::PixelFormat,
-    request::ReuseFlag,
-    stream::StreamRole,
-};
 use rppal::pwm;
 use tokio::{
     net,
     sync::{broadcast, mpsc},
 };
 
-type FrameData = Vec<u8>;
+use crate::footage::{footage_capture_task, FrameData};
+
+mod footage;
 
 enum ServoPosition {
     Left,
@@ -41,109 +26,8 @@ enum ServoPosition {
     Custom(f32),
 }
 
-const MAX_FRAME_RATE: f32 = 30.;
-const PIXEL_FORMAT: PixelFormat = PixelFormat::new(DrmFourcc::Bgr888 as u32, 0);
 const MAX_PULSE_WIDTH_US: u64 = 2000;
 const MIN_PULSE_WIDTH_US: u64 = 1000;
-
-fn footage_capture_task(footage_tx: Arc<broadcast::Sender<FrameData>>) {
-    // Initialize camera
-    let cam_mgr = CameraManager::new().unwrap();
-    let cameras = cam_mgr.cameras();
-    let cam = cameras.get(0).unwrap();
-    let mut cam = cam.acquire().unwrap();
-    let size = Size {
-        width: 1024,
-        height: 768,
-    };
-
-    // Configure camera
-    let mut cfgs = cam
-        .generate_configuration(&[StreamRole::VideoRecording])
-        .unwrap();
-    cfgs.get_mut(0).unwrap().set_pixel_format(PIXEL_FORMAT);
-    cfgs.get_mut(0).unwrap().set_size(size);
-
-    match cfgs.validate() {
-        CameraConfigurationStatus::Valid => println!("Camera configuration valid!"),
-        CameraConfigurationStatus::Adjusted => {
-            println!("Camera configuration was adjusted: {:#?}", cfgs)
-        }
-        CameraConfigurationStatus::Invalid => panic!("Error validating camera configuration"),
-    }
-
-    cam.configure(&mut cfgs).unwrap();
-
-    // Allocate framebuffers
-    let mut alloc = FrameBufferAllocator::new(&cam);
-    let cfg = cfgs.get(0).unwrap();
-    let stream = cfg.stream().unwrap();
-    let buffers = alloc.alloc(&stream).unwrap();
-
-    let buffers = buffers
-        .into_iter()
-        .map(|buf| MemoryMappedFrameBuffer::new(buf).unwrap())
-        .collect::<Vec<_>>();
-
-    // Create requests and bind the allocated buffers
-    let reqs = buffers
-        .into_iter()
-        .enumerate()
-        .map(|(i, buf)| {
-            let mut req = cam.create_request(Some(i as u64)).unwrap();
-            req.add_buffer(&stream, buf).unwrap();
-            req
-        })
-        .collect::<Vec<_>>();
-
-    let (req_tx, req_rx) = std::sync::mpsc::channel();
-    cam.on_request_completed(move |req| {
-        req_tx.send(req).unwrap();
-    });
-
-    // Start camera and queue capture requests
-    cam.start(None).unwrap();
-    for req in reqs {
-        cam.queue_request(req).unwrap();
-    }
-
-    let mut converted_bytes = Vec::new();
-
-    let mut start = Instant::now();
-    let mut end = Instant::now();
-
-    loop {
-        std::thread::sleep(Duration::from_millis(
-            (1000. / MAX_FRAME_RATE - (end - start).as_millis() as f32) as u64,
-        ));
-
-        if footage_tx.receiver_count() < 1 {
-            std::thread::sleep(Duration::from_secs(1));
-            continue;
-        }
-
-        start = Instant::now();
-
-        let mut req = req_rx.recv().unwrap();
-
-        let framebuffer: &MemoryMappedFrameBuffer<FrameBuffer> = req.buffer(&stream).unwrap();
-        let planes = framebuffer.data();
-
-        let frame_data = planes.get(0).unwrap();
-
-        let img: RgbImage =
-            ImageBuffer::from_raw(size.width, size.height, frame_data.to_vec()).unwrap();
-        img.write_to(&mut Cursor::new(&mut converted_bytes), ImageFormat::Jpeg)
-            .unwrap();
-
-        _ = footage_tx.send(converted_bytes.clone());
-
-        req.reuse(ReuseFlag::REUSE_BUFFERS);
-        cam.queue_request(req).unwrap();
-
-        end = Instant::now();
-    }
-}
 
 async fn servo_task(mut servo_rx: mpsc::UnboundedReceiver<ServoPosition>, servo_pwm: pwm::Pwm) {
     loop {
